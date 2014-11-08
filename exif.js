4f6e9de6b802c593fa454e87bbec7d2516c1e718
Filip Leczynski <filip@hexmedia.pl>
2014-11-08 16:47:59 +0100

    Adds support for tags used by windows

--- exif.js
+++ exif.js
@@ -129,7 +129,11 @@
         0x0131 : "Software",
         0x013B : "Artist",
         0x8298 : "Copyright",
-        0x9c9e : "XPKeywords"
+        0x9c9b : "XPTitle",
+        0x9c9c : "XPComment",
+        0x9c9d : "XPAuthor",
+        0x9c9e : "XPKeywords",
+        0x9c9f : "XPSubject"
     };
 
     var GPSTags = EXIF.GPSTags = {
