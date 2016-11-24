673d5880512f62bf11ef236e97b9de5cac7c56d6
Rubén Carrasco <rubensegovia@gmail.com>
2016-11-24 17:36:10 +0100

    declare variable properly inside a for loop, so the library doesn’t break when the code is uglified

--- exif.js
+++ exif.js
@@ -642,7 +642,7 @@
 
     function getStringFromDB(buffer, start, length) {
         var outstr = "";
-        for (n = start; n < start+length; n++) {
+        for (var n = start; n < start+length; n++) {
             outstr += String.fromCharCode(buffer.getUint8(n));
         }
         return outstr;
