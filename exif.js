4d1f723b5036ce822f92979760fea6d4c4895e30
Peter Litvak <peter.litvak@gmail.com>
2017-06-07 11:41:39 -0400

    Fixing a bug in getData method

--- exif.js
+++ exif.js
@@ -928,10 +928,10 @@
     }
 
     EXIF.getData = function(img, callback) {
-        if ((self.Image && img instanceof self.Image)
-            || (self.HTMLImageElement && img instanceof self.HTMLImageElement)
-            && !img.complete)
-            return false;
+        if (((self.Image && img instanceof self.Image) || (self.HTMLImageElement && img instanceof self.HTMLImageElement))
+            && !img.complete) {
+          return false;
+        }
 
         if (!imageHasData(img)) {
             getImageData(img, callback);
