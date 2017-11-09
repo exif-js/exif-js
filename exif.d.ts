293388500a9d4b35b1a5a1541267ed2700dea3b4
zGwit <jason@zgwit.com>
2017-11-09 20:15:12 +0800

    Update exif.d.ts
    Type error

--- exif.d.ts
+++ exif.d.ts
@@ -1,5 +1,5 @@
 interface EXIFStatic {
-    getData(url: string, callback: any): any;
+    getData(img: any, callback: any): any;
     getTag(img: any, tag: any): any;
     getAllTags(img: any): any;
     pretty(img: any): string;
