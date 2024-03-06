7d0ad1cfe5334be549d732d9e970af64ae78dcf4
Sergey Shabanov <shsv382@gmail.com>
2024-03-07 02:50:34 +0300

    Update exif.js: added LensModel Tag

--- exif.js
+++ exif.js
@@ -89,6 +89,7 @@
         0xA40A : "Sharpness",               // Direction of sharpness processing applied by camera
         0xA40B : "DeviceSettingDescription",    //
         0xA40C : "SubjectDistanceRange",    // Distance to subject
+        0xA434 : "LensModel",               // Lens Model
 
         // other tags
         0xA005 : "InteroperabilityIFDPointer",
